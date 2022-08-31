c8e8aa932338fea178927998e500c7311e32e79d
atorigin <qaz60908@gmail.com>
2022-08-31 21:20:07 +0800

    新增註解

--- exercise/fileupload/TcpServer.java
+++ exercise/fileupload/TcpServer.java
@@ -8,11 +8,15 @@ import java.net.Socket;
 
 public class TcpServer {
     public static void main(String[] args) throws Exception {
+        // 服務器端流程
+        // 1. 創建 Socket 要監聽某個 port
+        // 2. 等待客戶端連接
+        // 3. 建立 input stream 用來接收客戶的輸出資料流
         ServerSocket serverSocket = new ServerSocket(9000);
         Socket socket = serverSocket.accept();
         InputStream is = socket.getInputStream();
         
-        // 接收檔案並輸出
+        // 4. 建立檔案輸出流 , 要把收到的資料流寫成本地檔案用
         FileOutputStream fos = new FileOutputStream(new File("recieved.png"));
         byte[] buffer = new byte[1024];
         int len;
@@ -20,7 +24,9 @@ public class TcpServer {
             fos.write(buffer,0,len);
         }
 
+        // 關閉 straem
         fos.close();
+        is.close();
         socket.close();
         serverSocket.close();
     }
